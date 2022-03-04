7c751b679dac29d55b9f3e8de74f7d5785f1d006
davidmusembi <davidmusembi02@gmail.com>
2022-03-04 20:51:58 +0300

    corrected the placeholder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var akan = function(y, m, d, g) {
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var d = new Date(y, --m, d);
     if (g === "Female") {
         return d && femaleNames[d.getDay()];
